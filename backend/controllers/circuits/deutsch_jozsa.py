1bb1f35262ddd095659d2923b98aef38879ca49e
AndreeaStancu <77406144+AndreeaStancu@users.noreply.github.com>
2024-12-15 22:58:25 +0200

    Update deutsch_jozsa.py

--- backend/controllers/circuits/deutsch_jozsa.py
+++ backend/controllers/circuits/deutsch_jozsa.py
@@ -3,7 +3,7 @@ import cirq
 def create_deutsch_jozsa_circuit():
     qubits = [cirq.GridQubit(i, 0) for i in range(4)]
     circuit = cirq.Circuit()
-    circuit.append(cirq.H.on_each(qubits[:3]))
+    circuit.append(cirq.H(qubits[0]),cirq.H(qubits[1]),cirq.H(qubits[2]))
     circuit.append(cirq.X(qubits[1]))
     circuit.append(cirq.X(qubits[3]),cirq.H(qubits[3]))
     circuit.append(cirq.CNOT(qubits[0], qubits[3]))
